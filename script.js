9e94a0e0ac7b870717e61bd253f1f56697c4ad81
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2026-04-07 06:01:57 +0300

    completed step #3 of library project
    added a function that loops through the array and
     displays each book on the page
    
    added a card display for each book

--- script.js
+++ script.js
@@ -14,8 +14,24 @@ function createBook(author,title,pages){
   const book=new Book(author,title,pages)
   booksArray.push(book)
 }
-// createBook("John","success","10");
-// createBook("Mary","fail","100");
+createBook("John","success","10");
+createBook("Mary","fail","100");
 // console.log(booksArray)
 
-//
+//function that loops through the array 
+// and displays each book on the page.
+let libraryWrapper =document.getElementById("library");
+
+for(let i=0;i<booksArray.length;i++){
+  //create new div for each book
+  let bookCard = document.createElement("div")
+   bookCard.id="card"
+  bookCard.innerHTML=`
+   <h3>${booksArray[i].title}</h3>
+   <p> Author: ${booksArray[i].author}</p>
+   <p> Pages: ${booksArray[i].pages}</p>
+   <p> Id: ${booksArray[i].id}</p>
+   `
+  //append bookcards to librayWrapper
+  libraryWrapper.appendChild(bookCard)
+}
